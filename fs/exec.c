c421b103ee642c018d1d40d3bb3920dc524bf8d8
ravindu644 <Droidcasts@protonmail.com>
2024-08-18 21:59:17 +0530

    drivers: Introduced KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -72,6 +72,10 @@
 
 #include <trace/events/sched.h>
 
+#ifdef CONFIG_KSU
+#include <ksu_hook.h>
+#endif
+
 int suid_dumpable = 0;
 
 static LIST_HEAD(formats);
@@ -1740,6 +1744,10 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+
+#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif	
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
